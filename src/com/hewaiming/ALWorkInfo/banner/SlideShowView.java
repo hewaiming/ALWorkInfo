b27a7d8bbbf65bc26b19efc3097937b323c09a1b
Administrator <Administrator@USER-20160427JP>
2016-05-31 16:30:44 +0800

    banner

--- src/com/hewaiming/ALWorkInfo/banner/SlideShowView.java
+++ src/com/hewaiming/ALWorkInfo/banner/SlideShowView.java
@@ -1,5 +1,6 @@
 package com.hewaiming.ALWorkInfo.banner;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
@@ -25,223 +26,232 @@ import android.widget.LinearLayout;
 
 import com.hewaiming.ALWorkInfo.banner.JazzyViewPager.TransitionEffect;
 import com.hewaiming.allwork.R;
+import com.nostra13.universalimageloader.cache.disc.impl.UnlimitedDiscCache;
 import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.cache.memory.impl.UsingFreqLimitedMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
-
+import com.nostra13.universalimageloader.core.download.BaseImageDownloader;
+import com.nostra13.universalimageloader.utils.StorageUtils;
 
 /**
- * ViewPager实现的轮播图广告自定义视图，如京东首页的广告轮播图效果；
- * 既支持自动轮播页面也支持手势滑动切换页面
+ * ViewPager实现的轮播图广告自定义视图，如京东首页的广告轮播图效果； 既支持自动轮播页面也支持手势滑动切换页面
  *
  */
 
 public class SlideShowView extends FrameLayout {
-	
+
 	private JazzyViewPager mJazzy;
-	
+
 	// 使用universal-image-loader插件读取网络图片，需要工程导入universal-image-loader-1.8.6-with-sources.jar
 	private ImageLoader imageLoader = ImageLoader.getInstance();
 
-    //自动轮播的时间间隔
-    private final static int TIME_INTERVAL = 3;
-    //自动轮播启用开关
-    private final static boolean isAutoPlay = true; 
-    // 滚动框是否滚动着  
-    private boolean isScrolling = false;
-    
-    //自定义轮播图的资源
-    private List<String> imageUrls;
-    //放轮播图片的ImageView 的list
-    private List<ImageView> imageViewsList;
-    //放圆点的View的list
-    private List<View> dotViewsList;
-    
-    //当前轮播页
-    private int currentItem = 1;
-    //定时任务
-    private ScheduledExecutorService scheduledExecutorService;
-    
-    private Context context;
-    
-    //Handler
-    private Handler handler = new Handler(){
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            mJazzy.setCurrentItem(currentItem);
-        }
-    };
-    
-    public SlideShowView(Context context) {
-        this(context,null);
-    }
-    public SlideShowView(Context context, AttributeSet attrs) {
-        this(context, attrs, 0);
-    }
-    public SlideShowView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        this.context = context;
+	// 自动轮播的时间间隔
+	private final static int TIME_INTERVAL = 5;
+	// 自动轮播启用开关
+	private final static boolean isAutoPlay = true;
+	// 滚动框是否滚动着
+	private boolean isScrolling = false;
+
+	// 自定义轮播图的资源
+	private List<String> imageUrls;
+	// 放轮播图片的ImageView 的list
+	private List<ImageView> imageViewsList;
+	// 放圆点的View的list
+	private List<View> dotViewsList;
+
+	// 当前轮播页
+	private int currentItem = 1;
+	// 定时任务
+	private ScheduledExecutorService scheduledExecutorService;
+
+	private Context context;
+
+	// Handler
+	private Handler handler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			super.handleMessage(msg);
+			mJazzy.setCurrentItem(currentItem);
+		}
+	};
+
+	public SlideShowView(Context context) {
+		this(context, null);
+	}
+
+	public SlideShowView(Context context, AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
+
+	public SlideShowView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		this.context = context;
 		initImageLoader(context);
-        initData();
-        if(isAutoPlay){
-            startPlay();
-        }
-        
-    }
-    /**
-     * 开始轮播图切换
-     */
-    private void startPlay(){
-        scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
-        scheduledExecutorService.scheduleAtFixedRate(new SlideShowTask(), 1, TIME_INTERVAL, TimeUnit.SECONDS);
-    }
-    /**
-     * 停止轮播图切换
-     */
-    private void stopPlay(){
-        scheduledExecutorService.shutdown();
-    }
-    /**
-     * 初始化相关Data
-     */
-    private void initData(){
-    	imageUrls = new ArrayList<String>();
-        imageViewsList = new ArrayList<ImageView>();
-        dotViewsList = new ArrayList<View>();
-        //TODO 异步任务获取图片，这里可以改写为通过网络获取图片地址
-        new GetListTask().execute("");
-    }
-    /**
-     * 初始化Views等UI
-     */
-    private void initUI(Context context){
-    	if(imageUrls == null || imageUrls.size() == 0)
-    		return;
-        LayoutInflater.from(context).inflate(R.layout.layout_slideshow, this, true);
-        
-        LinearLayout dotLayout = (LinearLayout)findViewById(R.id.dotLayout);
-        dotLayout.removeAllViews();
-        
-        // 热点个数与图片特殊相等
-        for (int i = 0; i < imageUrls.size(); i++) {
-        	ImageView view =  new ImageView(context);
-        	view.setTag(imageUrls.get(i));
-        	view.setBackgroundResource(R.drawable.banner_default);
-        	view.setScaleType(ScaleType.FIT_XY);
-        	imageViewsList.add(view);
-        	
-        	ImageView dotView =  new ImageView(context);
-        	LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(android.view.ViewGroup.LayoutParams.WRAP_CONTENT,android.view.ViewGroup.LayoutParams.WRAP_CONTENT);
-        	params.leftMargin = 4;
+		initData();
+		if (isAutoPlay) {
+			startPlay();
+		}
+
+	}
+
+	/**
+	 * 开始轮播图切换
+	 */
+	private void startPlay() {
+		scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+		scheduledExecutorService.scheduleAtFixedRate(new SlideShowTask(), 1, TIME_INTERVAL, TimeUnit.SECONDS);
+	}
+
+	/**
+	 * 停止轮播图切换
+	 */
+	private void stopPlay() {
+		scheduledExecutorService.shutdown();
+	}
+
+	/**
+	 * 初始化相关Data
+	 */
+	private void initData() {
+		imageUrls = new ArrayList<String>();
+		imageViewsList = new ArrayList<ImageView>();
+		dotViewsList = new ArrayList<View>();
+		// TODO 异步任务获取图片，这里可以改写为通过网络获取图片地址
+		new GetListTask().execute("");
+	}
+
+	/**
+	 * 初始化Views等UI
+	 */
+	private void initUI(Context context) {
+		if (imageUrls == null || imageUrls.size() == 0)
+			return;
+		LayoutInflater.from(context).inflate(R.layout.layout_slideshow, this, true);
+
+		LinearLayout dotLayout = (LinearLayout) findViewById(R.id.dotLayout);
+		dotLayout.removeAllViews();
+
+		// 热点个数与图片特殊相等
+		for (int i = 0; i < imageUrls.size(); i++) {
+			ImageView view = new ImageView(context);
+			view.setTag(imageUrls.get(i));
+			view.setBackgroundResource(R.drawable.banner_default);
+			view.setScaleType(ScaleType.FIT_XY);
+			imageViewsList.add(view);
+
+			ImageView dotView = new ImageView(context);
+			LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+					android.view.ViewGroup.LayoutParams.WRAP_CONTENT, android.view.ViewGroup.LayoutParams.WRAP_CONTENT);
+			params.leftMargin = 4;
 			params.rightMargin = 4;
 			dotLayout.addView(dotView, params);
-        	dotViewsList.add(dotView);
+			dotViewsList.add(dotView);
 		}
-        //begin 这里主要是为了解决viewpager流畅循环问题，分别在最后一张图后加上第一张图、第一张图前加上最后一张图
-        ImageView viewFirst =  new ImageView(context);
-        viewFirst.setTag(imageUrls.get(imageUrls.size()-1));
-        viewFirst.setBackgroundResource(R.drawable.banner_default);
-        viewFirst.setScaleType(ScaleType.FIT_XY);
-        imageViewsList.add(0, viewFirst);
-        ImageView viewLast =  new ImageView(context);
-        viewLast.setTag(imageUrls.get(0));
-        viewLast.setBackgroundResource(R.drawable.banner_default);
-        viewLast.setScaleType(ScaleType.FIT_XY);
-        imageViewsList.add(viewLast);
-        //end
-        mJazzy = (JazzyViewPager) findViewById(R.id.jazzy_pager);
-        //TODO 设置切换动画类型
-        mJazzy.setTransitionEffect(TransitionEffect.Standard);      
+		// begin 这里主要是为了解决viewpager流畅循环问题，分别在最后一张图后加上第一张图、第一张图前加上最后一张图
+		ImageView viewFirst = new ImageView(context);
+		viewFirst.setTag(imageUrls.get(imageUrls.size() - 1));
+		viewFirst.setBackgroundResource(R.drawable.banner_default);
+		viewFirst.setScaleType(ScaleType.FIT_XY);
+		imageViewsList.add(0, viewFirst);
+		ImageView viewLast = new ImageView(context);
+		viewLast.setTag(imageUrls.get(0));
+		viewLast.setBackgroundResource(R.drawable.banner_default);
+		viewLast.setScaleType(ScaleType.FIT_XY);
+		imageViewsList.add(viewLast);
+		// end
+		mJazzy = (JazzyViewPager) findViewById(R.id.jazzy_pager);
+		// TODO 设置切换动画类型
+		mJazzy.setTransitionEffect(TransitionEffect.Standard);
 		mJazzy.setAdapter(new MainAdapter());
 		mJazzy.setCurrentItem(1);
 		mJazzy.setPageMargin(0);
 		mJazzy.setOnPageChangeListener(new MyPageChangeListener());
-    }
-    
-    /**
-     * ViewPager的监听器
-     * 当ViewPager中页面的状态发生改变时调用
-     * 
-     */
-    private class MyPageChangeListener implements OnPageChangeListener{
-        @Override
-        public void onPageScrollStateChanged(int arg0) {
-        	switch (arg0) {
-        	case 1:// 正在滑动
-        		isScrolling = true;
-        		break;
-        	case 2:// 滑动完毕
-        		isScrolling = false;
-        		break;
-        	case 0:// 什么都没做
-        		//当从最后一个滑动至第一个或者从第一个滑动至最后一个后，默默地将当前view变为真正的第一个或最后一个，
-        		//使用setCurrentItem(int item, boolean smoothScroll) smoothScroll为flase时不会有切换动画，所以可以欺骗用户，使其感觉不到页面的切换
-        		if(mJazzy.getCurrentItem() == mJazzy.getAdapter().getCount() - 1) {
-        			mJazzy.setCurrentItem(1, false);
-        		}else if(mJazzy.getCurrentItem() == 0) {
-        			mJazzy.setCurrentItem(mJazzy.getAdapter().getCount() - 2, false);
-        		}
-        		isScrolling = false;
-        		break;
-        	}
-        }
+	}
 
-        @Override
-        public void onPageScrolled(int arg0, float arg1, int arg2) {
-            
-        }
-
-        @Override
-        public void onPageSelected(int pos) {
-        	//由于adapter中实际有n+2张图片，而圆点的View中只有n个圆点，所以需要处理pos与i之间的对应关系
-            currentItem = pos;
-            if(pos == 0) {
-            	pos = dotViewsList.size() - 1;
+	/**
+	 * ViewPager的监听器 当ViewPager中页面的状态发生改变时调用
+	 * 
+	 */
+	private class MyPageChangeListener implements OnPageChangeListener {
+		@Override
+		public void onPageScrollStateChanged(int arg0) {
+			switch (arg0) {
+			case 1:// 正在滑动
+				isScrolling = true;
+				break;
+			case 2:// 滑动完毕
+				isScrolling = false;
+				break;
+			case 0:// 什么都没做
+					// 当从最后一个滑动至第一个或者从第一个滑动至最后一个后，默默地将当前view变为真正的第一个或最后一个，
+					// 使用setCurrentItem(int item, boolean smoothScroll)
+					// smoothScroll为flase时不会有切换动画，所以可以欺骗用户，使其感觉不到页面的切换
+				if (mJazzy.getCurrentItem() == mJazzy.getAdapter().getCount() - 1) {
+					mJazzy.setCurrentItem(1, false);
+				} else if (mJazzy.getCurrentItem() == 0) {
+					mJazzy.setCurrentItem(mJazzy.getAdapter().getCount() - 2, false);
+				}
+				isScrolling = false;
+				break;
 			}
-			else if(pos == mJazzy.getAdapter().getCount() - 1) {
+		}
+
+		@Override
+		public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+		}
+
+		@Override
+		public void onPageSelected(int pos) {
+			// 由于adapter中实际有n+2张图片，而圆点的View中只有n个圆点，所以需要处理pos与i之间的对应关系
+			currentItem = pos;
+			if (pos == 0) {
+				pos = dotViewsList.size() - 1;
+			} else if (pos == mJazzy.getAdapter().getCount() - 1) {
 				pos = 0;
-			}
-			else {
+			} else {
 				pos -= 1;
 			}
-            for(int i=0;i < dotViewsList.size();i++){
-                if(i == pos){
-                    dotViewsList.get(pos).setBackgroundResource(R.drawable.dot_focus);
-                }else {
-                    dotViewsList.get(i).setBackgroundResource(R.drawable.dot_blur);
-                }
-            }
-        }
-    }
+			for (int i = 0; i < dotViewsList.size(); i++) {
+				if (i == pos) {
+					dotViewsList.get(pos).setBackgroundResource(R.drawable.dot_focus);
+				} else {
+					dotViewsList.get(i).setBackgroundResource(R.drawable.dot_blur);
+				}
+			}
+		}
+	}
 
-    /**
-     * JazzyViewPager 的适配器
-     * @author yuyang 2015年6月9日
-     *
-     */
-    private class MainAdapter extends PagerAdapter {
-    	
+	/**
+	 * JazzyViewPager 的适配器
+	 * 
+	 * @author yuyang 2015年6月9日
+	 *
+	 */
+	private class MainAdapter extends PagerAdapter {
+
 		@Override
 		public Object instantiateItem(ViewGroup container, int position) {
-            ImageView imageView = imageViewsList.get(position);
+			ImageView imageView = imageViewsList.get(position);
 			imageLoader.displayImage(imageView.getTag() + "", imageView);
-            ((ViewPager)container).addView(imageViewsList.get(position));
-            mJazzy.setObjectForPosition(imageViewsList.get(position), position);
-            return imageViewsList.get(position);
+			((ViewPager) container).addView(imageViewsList.get(position));
+			mJazzy.setObjectForPosition(imageViewsList.get(position), position);
+			return imageViewsList.get(position);
 		}
-		
+
 		@Override
 		public void destroyItem(ViewGroup container, int position, Object obj) {
 			container.removeView(mJazzy.findViewFromObject(position));
 		}
-		
+
 		@Override
 		public int getCount() {
 			return imageViewsList.size();
 		}
-		
+
 		@Override
 		public boolean isViewFromObject(View view, Object obj) {
 			if (view instanceof OutlineContainer) {
@@ -249,42 +259,42 @@ public class SlideShowView extends FrameLayout {
 			} else {
 				return view == obj;
 			}
-		}		
+		}
 	}
-    
-    /**
-     *执行轮播图切换任务
-     *
-     */
-    private class SlideShowTask implements Runnable{
 
-        @Override
-        public void run() {
-            synchronized (mJazzy) {
-            	if (!isScrolling) {
-            		currentItem = (currentItem+1)%imageViewsList.size();
-            		handler.obtainMessage().sendToTarget();
-            	}
-            }
-        }
-        
-    }
-    
-    /**
-     * 销毁ImageView资源，回收内存
-     * 
-     */
-    private void destoryBitmaps() {
+	/**
+	 * 执行轮播图切换任务
+	 *
+	 */
+	private class SlideShowTask implements Runnable {
 
-        for (int i = 0; i < imageViewsList.size(); i++) {
-            ImageView imageView = imageViewsList.get(i);
-            Drawable drawable = imageView.getDrawable();
-            if (drawable != null) {
-                //解除drawable对view的引用
-                drawable.setCallback(null);
-            }
-        }
-    }
+		@Override
+		public void run() {
+			synchronized (mJazzy) {
+				if (!isScrolling) {
+					currentItem = (currentItem + 1) % imageViewsList.size();
+					handler.obtainMessage().sendToTarget();
+				}
+			}
+		}
+
+	}
+
+	/**
+	 * 销毁ImageView资源，回收内存
+	 * 
+	 */
+	private void destoryBitmaps() {
+
+		for (int i = 0; i < imageViewsList.size(); i++) {
+			ImageView imageView = imageViewsList.get(i);
+			Drawable drawable = imageView.getDrawable();
+			if (drawable != null) {
+				// 解除drawable对view的引用
+				drawable.setCallback(null);
+			}
+		}
+	}
 
 	/**
 	 * 异步任务,获取数据
@@ -311,11 +321,11 @@ public class SlideShowView extends FrameLayout {
 		protected void onPostExecute(Boolean result) {
 			super.onPostExecute(result);
 			if (result) {
-		        initUI(context);
+				initUI(context);
 			}
 		}
 	}
-	
+
 	/**
 	 * ImageLoader 图片组件初始化
 	 * 
@@ -327,13 +337,40 @@ public class SlideShowView extends FrameLayout {
 		// or you can create default configuration by
 		// ImageLoaderConfiguration.createDefault(this);
 		// method.
-		ImageLoaderConfiguration config = 
-				new ImageLoaderConfiguration.Builder(context)
-					.threadPriority(Thread.NORM_PRIORITY - 2)
-					.denyCacheImageMultipleSizesInMemory()
-					.discCacheFileNameGenerator(new Md5FileNameGenerator())
-					.tasksProcessingOrder(QueueProcessingType.LIFO).writeDebugLogs()
-					.build();
+		File cacheDir = StorageUtils.getOwnCacheDirectory(context, "imageloader/Cache");
+		ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context)
+				.memoryCacheExtraOptions(480, 800) // maxwidth, max
+													// height，即保存的每个缓存文件的最大长宽
+				.threadPoolSize(3)// 线程池内加载的数量
+				.threadPriority(Thread.NORM_PRIORITY - 2).denyCacheImageMultipleSizesInMemory()
+				.memoryCache(new UsingFreqLimitedMemoryCache(2 * 1024 * 1024)) // You  can pass your own memory cache													
+																																																								
+																		
+																				// implementation/你可以通过自己的内存缓存实现
+				.memoryCacheSize(2 * 1024 * 1024).discCacheSize(50 * 1024 * 1024)
+
+				.tasksProcessingOrder(QueueProcessingType.LIFO).discCacheFileCount(100) // 缓存的文件数量
+				.discCache(new UnlimitedDiscCache(cacheDir))// 自定义缓存路径
+				.defaultDisplayImageOptions(DisplayImageOptions.createSimple())
+				.imageDownloader(new BaseImageDownloader(context, 5 * 1000, 30 * 1000)) // connectTimeout
+																						// (5
+																						// s),
+																						// readTimeout
+																						// (30
+																						// s)超时时间
+				.writeDebugLogs() // Remove for releaseapp
+				.build();// 开始构建
+
+		/*
+		 * ImageLoaderConfiguration config = new
+		 * ImageLoaderConfiguration.Builder(context)
+		 * .threadPriority(Thread.NORM_PRIORITY - 2)
+		 * .denyCacheImageMultipleSizesInMemory()
+		 * 
+		 * .discCacheFileNameGenerator(new Md5FileNameGenerator())
+		 * .tasksProcessingOrder(QueueProcessingType.LIFO).writeDebugLogs()
+		 * .build();
+		 */
 		// Initialize ImageLoader with configuration.
 		ImageLoader.getInstance().init(config);
 	}
--- src/com/hewaiming/ALWorkInfo/ui/MainActivity.java
+++ src/com/hewaiming/ALWorkInfo/ui/MainActivity.java
@@ -6,11 +6,6 @@ import java.util.List;
 import java.util.Map;
 
 import com.hewaiming.allwork.R;
-import com.hwm.hiDemo.ContactsActivity;
-import com.hwm.hiDemo.MainActivity;
-import com.hwm.hiDemo.NewsActivity;
-import com.hwm.hiDemo.YoutubeActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -30,7 +25,7 @@ public class MainActivity extends Activity implements OnItemClickListener {
 	private SimpleAdapter adapter;
 	private List<Map<String, Object>> dataList;
 	private String[] iconName = { "常用参数", "日报", "效应报表", "槽龄", "槽压曲线", "故障表", "解析记录", "修改参数记录", 
-			"槽状态","测量数据", "效应次数最多", "效应时间最长","故障最多","下料异常", "报警记录" ,"实时曲线"};
+			"槽状态","测量数据", "效应次数最多", "效应时间最长","槽故障最多","下料异常", "报警记录" ,"实时曲线"};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +38,7 @@ public class MainActivity extends Activity implements OnItemClickListener {
 	private void init() {
 		gridView = (GridView) findViewById(R.id.gridView);
 		dataList = new ArrayList<Map<String, Object>>();
-		adapter = new SimpleAdapter(this, getData(), R.layout.item, new String[] { "pic", "name" },
+		adapter = new SimpleAdapter(this, getData(), R.layout.item_action, new String[] { "pic", "name" },
 				new int[] { R.id.pic, R.id.name });
 		gridView.setAdapter(adapter);
 		gridView.setOnItemClickListener(this);
@@ -52,9 +47,10 @@ public class MainActivity extends Activity implements OnItemClickListener {
 
 	private List<Map<String, Object>> getData() {
 
-		int[] drawable = { R.drawable.address_book, R.drawable.calendar, R.drawable.camera, R.drawable.clock,
-				R.drawable.games_control, R.drawable.messenger, R.drawable.ringtone, R.drawable.settings,
-				R.drawable.speech_balloon, R.drawable.weather, R.drawable.world, R.drawable.youtube, R.drawable.news };
+		int[] drawable = { R.drawable.set_params, R.drawable.day_table, R.drawable.ae_table, R.drawable.pot_age,
+				R.drawable.potv, R.drawable.fault_list, R.drawable.jx_record, R.drawable.update_params,
+				R.drawable.pot_status, R.drawable.measure, R.drawable.ae_nums, R.drawable.ae_long,
+				R.drawable.fault_more,R.drawable.nb_abnorm ,R.drawable.alarm_list,R.drawable.real_potv};
 		for (int i = 0; i < drawable.length; i++) {
 			Map<String, Object> map = new HashMap<String, Object>();
 			map.put("pic", drawable[i]);
@@ -86,18 +82,18 @@ public class MainActivity extends Activity implements OnItemClickListener {
 	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 		switch (position) {
 		case 0:
-			Intent phone_intent = new Intent(MainActivity.this, ContactsActivity.class);
-			startActivity(phone_intent);
+//			Intent phone_intent = new Intent(MainActivity.this, ContactsActivity.class);
+//			startActivity(phone_intent);
 			break;
 		case 1:
 			break;
 		case 11:
-			Intent youtubeIntent = new Intent(MainActivity.this, YoutubeActivity.class);
-			startActivity(youtubeIntent);
+//			Intent youtubeIntent = new Intent(MainActivity.this, YoutubeActivity.class);
+//			startActivity(youtubeIntent);
 			break;
 		case 12:
-			Intent news_intent = new Intent(MainActivity.this, NewsActivity.class);
-			startActivity(news_intent);
+//			Intent news_intent = new Intent(MainActivity.this, NewsActivity.class);
+//			startActivity(news_intent);
 			break;
 		}
 
